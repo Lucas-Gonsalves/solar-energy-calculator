254ec71bedd029b33936f16293cc489ac8b68664
lucas <lucasluz1710@gmail.com>
2024-02-16 22:34:38 -0300

    feat(styles) global   Created the base global styling of the application and imported it into the App.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,15 @@ import { defaultTheme } from "./styles/themes/default";
 import { ThemeProvider } from "styled-components";
 
 import { Routes } from "./routes";
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      
       <Routes/>
     </ThemeProvider>
   );
